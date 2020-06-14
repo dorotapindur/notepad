ff98355a8278d817c374d3a59991986afb1867dc
Dorota Pindur <dpindur@wp.pl>
2020-06-14 23:04:46 +0200

    buttons working

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,29 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log(`HELLO 🚀 I'm trying to make a notepad`);
+
+const textarea = document.querySelector('.text--js');
+
+const saveButton = document.querySelector('.save--js');
+
+const loadButton = document.querySelector('.load--js');
+
+const clearButton = document.querySelector('.clear--js');
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('saved', textarea.value);
+    console.log("saved");
+    
+});
+
+loadButton.addEventListener('click', () => {  
+    textarea.value = localStorage.getItem('saved', textarea.value);   
+    console.log('loaded');
+});
+
+clearButton.addEventListener('click', () => {  
+    textarea.value = '';   
+    console.log('cleared');
+});
+
